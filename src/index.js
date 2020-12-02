3e164457074e1c510a9adca4cab0cb1ec231984e
井上凌 <69154468+inoueryou@users.noreply.github.com>
2020-12-03 03:07:36 +0900

    変数

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言！！";
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+//letは上書が可能。
+val2 = "let変数を上書き";
+console.log(val2);
